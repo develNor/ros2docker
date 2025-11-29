2eb0949e5213ded62a0a63a9f0b565a534d377f2
Martin Gontscharow <gontscharow@fzi.de>
2025-11-29 10:56:51 +0100

    add config file arguments to stop.py

--- stop.py
+++ stop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import sys
 import subprocess
@@ -9,10 +10,16 @@ sys.path.append(project_dir)
 
 from utils.getters import *
 
-def main():
-    docker_command = [ 'docker', 'stop', get_container_name() ]
+def main(config_file=None, override=None):
+    docker_command = [ 'docker', 'stop', get_container_name(config_file, override) ]
     print("Executing Docker command:", ' '.join(docker_command))
     subprocess.run(docker_command)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run a Docker container with specified arguments.")
+    parser.add_argument('-o', '--override')
+    parser.add_argument('-f', '--config_file')
+    args = parser.parse_args()
+
+    # Use **vars(args) to convert argparse.Namespace to a dict, filtering out None values
+    main(**{k: v for k, v in vars(args).items() if v is not None})
